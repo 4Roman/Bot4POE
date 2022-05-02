9b63c7ff613ef398783c3c60f24a2d6ee1833670
4Roman <63200666+4Roman@users.noreply.github.com>
2022-05-02 19:32:59 +0300

    Добавьте файлы проекта.

---
+++ Bot4POE/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bot4POE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap test = (Bitmap)Screenshot.MakeScreenshot().Clone(); 
+            Console.WriteLine(test.Width);
+            Console.WriteLine(test.Height);
+        }
+    }
+}
---
+++ Bot4POE/Screenshot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+namespace Bot4POE
+{
+    public static class Screenshot
+    {
+        public static Bitmap MakeScreenshot()
+        {            
+            Rectangle bounds = Screen.GetBounds(Point.Empty);// получаем размеры окна рабочего стола            
+            using (Bitmap bitmap = new Bitmap(bounds.Width, bounds.Height))// создаем пустое изображения размером с экран устройства
+            {
+                using (var g = Graphics.FromImage(bitmap))// создаем объект на котором можно рисовать
+                {
+                    g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size);// перерисовываем экран на наш графический объект
+                }
+                bitmap.Save("screenshot_01.jpg", ImageFormat.Jpeg);// сохраняем в файл с форматом JPG
+                return bitmap;                
+            }
+        }
+        public static Bitmap Crop(Bitmap image)
+        {
+            Rectangle rectangle = new Rectangle(0,image.Height*3/4,image.Width/5,image.Height/4);
+            Bitmap pic = new Bitmap(image);
+            pic = pic.Clone(rectangle, PixelFormat.Format16bppRgb555);
+            pic.Save("screenshot_02.jpg", ImageFormat.Jpeg);// сохраняем в файл с форматом JPG
+            return pic;
+
+        }
+    }
+
+
+}
